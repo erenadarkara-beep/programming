9dfc96bc369264e5fb71d4a13e6e5140b82f5fde
erenadarkara-beep <erenadarkara@gmail.com>
2025-09-08 12:27:25 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int Control(int altitude) {
+  int thruster;
+
+  if (altitude > 100) {
+    thruster = 0;
+  } else if (altitude > 0) {
+    thruster = 1;
+  } else {
+    thruster = 0;
+  }
+
+  return thruster;
+}
+
+void Test(int altitude) {
+  int thruster = Control(altitude);
+  int behaviorCorrect = (altitude > 100 && thruster == 0) ||
+                        (altitude <= 100 && altitude > 0 && thruster == 1) ||
+                        (altitude <= 0 && thruster == 0);
+
+  char *behaviorCorrectIcon;
+  if (behaviorCorrect) {
+    behaviorCorrectIcon = "OK";
+  } else {
+    behaviorCorrectIcon = "FAIL";
+  }
+
+  printf("For altitude %3d, your thruster is %d |%s|\n",
+         altitude, thruster, behaviorCorrectIcon);
+}
+
+int main(void) {
+  Test(150);
+  Test(100);
+  Test(50);
+  Test(0);
+  Test(-1);
+
+  return 0;
+}
